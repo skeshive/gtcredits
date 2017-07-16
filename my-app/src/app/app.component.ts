46f37463d9a11a824156309f260727c0986ff7c0
Shreya Keshive <shreyakeshive@gatech.edu>
2017-07-16 13:16:57 -0400

    Routing now works

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 
+import { ConvertScoreService } from './services/convert-score.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 
-export class AppComponent { }
+export class AppComponent {
+  constructor(private _convertScore: ConvertScoreService) { }
+}
